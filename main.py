072182c7ba489a7fff037650b7e73443f17e0adf
EduardoKing1813 <KingEdward1813@yandex.ru>
2022-04-29 22:24:06 +0200

    Implemented settings frame

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from logging import root
 from tkinter import *
-from modules.root_widgets import RootWelcomeLabel
+from modules.root_widgets import RootWelcomeLabel, SettingsFrame
 
 def main():
     #Root window init
     root = Tk()
     root.title("TypeFast!")
-    root.geometry("400x300")
+    #root.geometry("400x300")
     root.resizable(width=False, height=False)
     
     welcome_label = RootWelcomeLabel(root)
-    welcome_label.grid(0,0)
+    welcome_label.grid(row=0, column=1)
+    
+    settings_frame = SettingsFrame(root)
+    settings_frame.grid(row=1, column=0)
     
     #Main loop
     root.mainloop()
-    
-    
-    
-    
-    
-    
-    
-    pass
+
 
 
 if __name__ == '__main__':
--- modules/root_widgets.py
+++ modules/root_widgets.py
@@ -1,11 +1,11 @@
-from this import d
-from tkinter import Label
+from tkinter import Label, Radiobutton
+from tkinter.ttk import LabelFrame, Combobox
 
 
 class RootWidget:
     
-    def grid(self, r, c):
-        self.widget.grid(row=r, column=c)
+    def grid(self, row, column):
+        self.widget.grid(row=row, column=column)
 
 
 class RootWelcomeLabel(RootWidget):
@@ -13,5 +13,45 @@ class RootWelcomeLabel(RootWidget):
     def __init__(self, root):
         self.root = root
         self.widget = Label(root, text="TypeFast!")
+        
+        
+class SettingsFrame(RootWidget):
+    
+    def __init__(self, root):
+        self.root = root
+        
+        #Creating widget body
+        self.widget = LabelFrame(root, text="Settings")
+        
+        #Widgets for text seletion
+        self.label_text = Label(self.widget, text="Text:")
+        self.label_text.grid(row=0, column=0, sticky="W")
+        
+        self.combobox_text_selection = Combobox(self.widget)
+        self.combobox_text_selection['values'] = ['a', 'b', 'c']
+        self.combobox_text_selection.grid(row=1, column=0, pady=(0, 30))
+        
+        #Widgets for case sensitivity selection
+        self.label_case_sensitivity_selection = Label(self.widget, text="Is case sensitive:")
+        self.label_case_sensitivity_selection.grid(row=2, column=0, sticky="W")
+        
+        self.radio_buttons_case_sensitivity_selection_items = {"Yes" : True, "No (easy)" : False}
+        self.radio_buttons_case_sensitivity_selection = []
+        
+        for (text, value) in self.radio_buttons_case_sensitivity_selection_items.items():
+            self.radio_buttons_case_sensitivity_selection.append( Radiobutton(self.widget, text=text, value=value) )
+        
+        for index, rb in enumerate(self.radio_buttons_case_sensitivity_selection):
+            rb.grid(row=3, column=index, sticky="W", pady=(0, 30))
+            
+        #Widgets for color selection
+        self.label_color_selection = Label(self.widget, text="Next character color:")
+        self.label_color_selection.grid(row=4, column=0, sticky="W")
+        
+        self.combobox_color_selection = Combobox(self.widget)
+        self.combobox_color_selection['values'] = ['Red', 'Green', 'Blue']
+        self.combobox_color_selection.grid(row=5, column=0, sticky="W", pady=(0, 60))
+        
+        
         
         
